1733d829d4bbd7a346d8d1e45d12857ec2496d9f
Anartz Mugika Ledo <mugan86@gmail.com>
2020-07-17 07:49:01 +0200

    Update API with new versions adapt

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 API GraphQL en la que trabajamos con las bases de datos relacionales MySQL siguiendo el siguiente diseño:
 
 ![Database](/screens/db.png)
+
+Las operaciones que realizaremos en este proyecto, son las siguientes:
+
+![Endpoints](./api.png)
+
+Espero que os ayude este ejemplo básico y obtengáis las nociones básicas necesarias para trabajar con Bases de datos relacionales.
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -2,8 +2,10 @@ import { GraphQLSchema } from 'graphql';
 import 'graphql-import-node';
 import resolvers from '../resolvers';
 import { makeExecutableSchema } from 'graphql-tools';
-import { fileLoader, mergeTypes } from 'merge-graphql-schemas';
-const typeDefs = mergeTypes(fileLoader(`${__dirname}/**/*.graphql`), { all: true });
+import { loadFilesSync } from '@graphql-tools/load-files';
+import { mergeTypeDefs } from '@graphql-tools/merge';
+const loadedFiles = loadFilesSync(`${__dirname}/**/*.graphql`);
+const typeDefs = mergeTypeDefs(loadedFiles);
 const schema: GraphQLSchema = makeExecutableSchema({
     typeDefs,
     resolvers
